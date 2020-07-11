6e616493dc55f90324b189eb7f89fc0702386430
ligorikus <semal551@gmail.com>
2020-07-11 14:48:24 +0600

    upd

--- app/Model/Transaction.php
+++ app/Model/Transaction.php
@@ -23,10 +23,10 @@ class Transaction extends Model
         return $this->belongsToMany(Product::class, 'product_expresses')->withPivot(['value' , 'type']);
     }
 
-    public function getCreatedAtAttribute($value)
+ /*   public function getCreatedAtAttribute($value)
     {
         $date = Carbon::createFromFormat('Y-m-d H:i:s', $value, 'UTC');
         $timezone = Config::get('app.timezone');
         return $date->setTimezone($timezone)->toDateTimeString();
-    }
+    }*/
 }
